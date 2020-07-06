3b1df907447db44943f4649b4b71c8aa2148a444
KKucher <kkucher92@yandex.ru>
2020-07-07 00:54:16 +0500

    restore project and add files

---
+++ script.js
@@ -0,0 +1,32 @@
+const name = document.querySelector('.profile__name');
+const work = document.querySelector('.profile__work');
+const editProfileButton = document.querySelector('.profile__btn_action_edit');
+const editProfilePopup = document.querySelector('.popup_content_profile');;
+const editProfileForm = editProfilePopup.querySelector('.popup__form');
+const nameInput = editProfileForm.querySelector('.popup__item_type_name');
+const workInput = editProfileForm.querySelector('.popup__item_type_info');
+const closeProfileButton = editProfilePopup.querySelector('.popup__btn_action_close');
+
+
+const togglePopup = function(popup) {
+  popup.classList.toggle('popup_opened');
+}
+
+const editProfile = function() {
+  togglePopup(editProfilePopup);
+  nameInput.value = name.textContent;
+  workInput.value = work.textContent;
+}
+
+const saveProfile = function(evt) {
+  evt.preventDefault();
+  name.textContent = nameInput.value;
+  work.textContent = workInput.value;
+  togglePopup(editProfilePopup);
+}
+
+
+editProfileButton.addEventListener('click', editProfile);
+editProfileForm.addEventListener('submit', saveProfile);
+
+closeProfileButton.addEventListener('click', () => togglePopup(editProfilePopup))
